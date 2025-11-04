0f9ce8bcff75176d042fc45d9f0c7f1a138a7b9c
vlu9449-art <vlu9449@gmail.com>
2025-11-04 12:04:46 +0800

    particle

--- particle-world/sketch.js
+++ particle-world/sketch.js
@@ -1,10 +1,10 @@
 // CCLab Mini Project - 9.R Particle World Template
 
 let NUM_OF_PARTICLES = 3; // Decide the initial number of particles.
-let MAX_OF_PARTICLES = 500; // Decide the maximum number of particles.
+let MAX_OF_PARTICLES = 200; // Decide the maximum number of particles.
 
 let particles = [];
-
+let n = 50;
 function setup() {
   let canvas = createCanvas(800, 500);
   canvas.parent("p5-canvas-container");
@@ -12,7 +12,7 @@ function setup() {
 
   // generate particles
   for (let i = 0; i < NUM_OF_PARTICLES; i++) {
-    particles[i] = new Particle(random(width), random(height));
+    particles[i] = new Particle(random(30, width - 30), random(30, height - 30));
   }
 }
 
@@ -36,10 +36,12 @@ function draw() {
 
 class Particle {
   // constructor function
-  constructor(startX, startY) {
+  constructor(x, y) {
     // properties (variables): particle's characteristics
-    this.x = startX;
-    this.y = startY;
+    this.x = x;
+    this.y = y;
+    this.x0 = x;
+    this.y0 = y;
     this.dia = 30;
   }
   // methods (functions): particle's behaviors
@@ -52,42 +54,36 @@ class Particle {
     translate(this.x, this.y);
     //circle(0, 0, this.dia);
 
-    let h = map(frameCount * 0.01, 0, 100, 10, 70);
+    let h = map(cos(frameCount * 0.005), -1, 1, 10, 70);
     let bs = map(frameCount * 0.02, 0, 100, 70, 100);
-    let t = map(frameCount * 0.02, 0, 100, 70, 90);
+    let t = map(frameCount * 0.02, 0, 100, 80, 90);
     stroke(h, 20, bs, t);
-    strokeWeight(1.5);
-    let c = map(cos(frameCount * 0.008), -1, 1, 0, 100);
+    strokeWeight(2);
+    let c = map(cos(frameCount * 0.02), -1, 1, 0, 100);
     let s = frameCount * 0.01 % 30;
-    let b = frameCount * 0.1;
-    if (b > 100) {
-      b = 100;
-    }
+    let b = map(frameCount * 0.1, 0, 100, 60, 90);
     fill(c, s, b, 20);
 
-    //creature
+    //particle
     beginShape();
     for (let i = 0; i < n; i++) {
-      //creature transformation with mouse
-      let f = map(d, 0, width, 0.8, 0.1);
-      let spikes = map(d, 0, width, 1, 20);
-      angle = map(i, 0, n, -2 * PI, 2 * PI);
+      //particle
+      let freq = map(frameCount * 0.05, 0, width, 0.1, 0.8);
+      let spikes = map(frameCount * 0.05, 0, height, 1, 5);
+      let angle = map(i, 0, n, -2 * PI, 2 * PI);
       let offset = map(i, 0, n, -spikes * PI, spikes * PI);
 
-      //spinning speed decreases with mouse distance
-      let sp = map(d, 0, width, 0.001, 0.02);
+      //spinning speed
+      let sp = map(cos(frameCount * 0.005), 0, 1, 0.05, 0.02);
 
-      //size & breathing animation of the creature
-      let p = 100 + 10 * sin(frameCount * 0.05);
-      let r = p + 10 * sin(frameCount * f * sp + offset);
+      //size & breathing animation of the particle
+      let p = this.dia + sin(frameCount * 0.05);
+      let r = p + 5 * sin(frameCount * freq * sp + offset);
 
-      let xs = 0;
-      let ys = 0;
+      this.x = this.x0 + r * cos(angle);
+      this.y = this.y0 + r * sin(angle);
 
-      x = xs + r * cos(angle);
-      y = ys + r * sin(angle);
-
-      curveVertex(x, y);
+      curveVertex(this.x, this.y);
     }
     endShape(CLOSE);
     pop();
