860d9356efc89b1cbdb5a9f8bdc8ebc3789e73eb
vlu9449-art <vlu9449@gmail.com>
2025-11-23 00:13:01 +0800

    b prototype final

--- project-b-prototype/canvas1/sketch.js
+++ project-b-prototype/canvas1/sketch.js
@@ -1,13 +1,15 @@
 let bg, pv;
 // let sounds = [];
 // let regions = [];
-let ship, wind;
+let ship, wind, wave, bell;
 let s = 30;
 function preload() {
   bg = loadImage("assets/SH-bg-painting.JPG");
   pv = loadImage("assets/promotion video substitute.jpg");
   ship = loadSound("assets/ships.mp3");
+  wave = loadSound("assets/wave.mp3");
   wind = loadSound("assets/flag.mp3");
+  bell = loadSound("assets/DongfangHong.mp3");
 }
 
 function setup() {
@@ -41,28 +43,40 @@ function draw() {
   image(bg, width / 2, height / 2, width, height + 100);
   image(pv, width / 2, height / 2 + 80, 720, 450);
 
-  let inShipRegion1 = mouseX >= 180 && mouseX <= 400 && mouseY >= 400 && mouseY <= 630;
-  let inShipRegion2 = mouseX >= 1050 && mouseX <= 1330 && mouseY >= 360 && mouseY <= 640;
-  let shipShouldPlay = inShipRegion1 || inShipRegion2;
+  //sound trigger in different areas
+  //river
+  let inRiverRegion1 = mouseX >= 160 && mouseX <= 500 && mouseY >= 390 && mouseY <= 580;
+  let inRiverRegion2 = mouseX >= 1210 && mouseX <= 1600 && mouseY >= 360 && mouseY <= 560;
+  let riverShouldPlay = inRiverRegion1 || inRiverRegion2;
 
-  let inWindRegion = mouseX >= 220 && mouseX <= 400 && mouseY >= 185 && mouseY <= 260;
-  // regions = [
-  //   { x: 50, y: 200, w: 50, h: 100, soundIndex: 0 },
-  //   { x: 800, y: 200, w: 100, h: 120, soundIndex: 0 },
-  //   { x: 50, y: 120, w: 20, h: 15, soundIndex: 1 }
-  // ];
-  if (shipShouldPlay && !ship.isPlaying()) {
+  if (riverShouldPlay && !ship.isPlaying() && !wave.isPlaying()) {
     ship.loop();
+    wave.loop();
   }
-  if (!shipShouldPlay && ship.isPlaying()) {
+  if (!riverShouldPlay && ship.isPlaying() && wave.isPlaying()) {
     ship.stop();
+    wave.stop();
   }
+
+  //flag
+  let inWindRegion = mouseX >= 230 && mouseX <= 350 && mouseY >= 185 && mouseY <= 250;
 
   if (inWindRegion && !wind.isPlaying()) {
     wind.loop();
   }
+
   if (!inWindRegion && wind.isPlaying()) {
     wind.stop();
+  }
+
+  //bell tower
+  let inBellTower = mouseX >= 165 && mouseX <= 315 && mouseY >= 550 && mouseY <= height;
+
+  if (inBellTower && !bell.isPlaying()) {
+    bell.play();
+  }
+  if (!inBellTower && bell.isPlaying()) {
+    bell.stop();
   }
 }
 
