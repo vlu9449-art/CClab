f36c884b01c5ec09b4bc42dc80687867dfa3cb68
vlu9449-art <vlu9449@gmail.com>
2025-12-06 23:14:04 +0800

    canvas2-1st version

--- Project_B_Final/canvas2/sketch.js
+++ Project_B_Final/canvas2/sketch.js
@@ -5,24 +5,32 @@ let imgH, imgW;
 let autoScrollSpeed = 3;
 let scrollZone = 150;
 
-// let handPose;
-// let video;
-// let hands = [];
-// let options = { maxHands: 1, flipped: false };
-// let p1 = 0;
-// let p2 = 0;
+let handPose;
+let video;
+let hands = [];
+let options = { maxHands: 1, flipped: false };
+let p1;
+let p2;
+
+let cameraMode = false;
 
 function preload() {
-  bg = loadImage("assets/Yulan.png");
+  bg = loadImage("assets/Yulan.JPG");
   for (let i = 1; i < 29; i++) {
     let fileName = 'assets/' + i + '.jpg';
     img.push(loadImage(fileName));
   }
-  //handPose = ml5.handPose(options);
+  handPose = ml5.handPose(options);
 }
 
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight * 1.28);
+  //let canvas = createCanvas(windowWidth * 1.2, windowHeight * 1.36);
+  let w = windowWidth * 1.2;
+  let h = windowHeight * 1.36;
+
+  let canvas = createCanvas(w, h);
+  // make the whole document at least as wide as the canvas
+  document.body.style.width = w + 'px';
   canvas.parent("p5-canvas-container");
   imageMode(CENTER);
 
@@ -37,28 +45,50 @@ function setup() {
     pix.push(new Pix(img[i], x, y, baseSpeed));
   }
 
-  // video = createCapture(VIDEO);
-  // video.size(windowWidth, windowHeight * 1.28);
-  // video.hide();
-
-  // Start detecting hands from the webcam video
-  //handPose.detectStart(video, gotHands);
+  video = createCapture(VIDEO);
+  video.size(windowWidth, windowHeight);
+  video.hide();
+  cameraMode = true;
+  //Start detecting hands from the webcam video
+  handPose.detectStart(video, gotHands);
 }
 
 // Callback function for when handPose outputs data
-// function gotHands(results) {
-//   // Save the output to the hands variable
-//   hands = results;
-// }
+function gotHands(results) {
+  // Save the output to the hands variable
+  hands = results;
+}
+
+function hideVideo() {
+  push();
+  translate(width / 2, height / 2);
+  scale(-1, 1);
+  image(video, 0, 0, width, height);
+  pop();
+}
 
 function draw() {
   background(225);
-  tint(255, 100);
+  if (cameraMode) {
+    hideVideo();
+  }
   image(bg, width / 2, height / 2, width, height);
 
-  noTint();
   for (let i = 0; i < img.length; i++) {
     pix[i].update();
     pix[i].display();
+  }
+
+  // Draw all tracked hand points
+  for (let i = 0; i < hands.length; i++) {
+    let hand = hands[i];
+    for (let j = 0; j < hand.keypoints.length; j++) {
+      p1 = hand.keypoints[8];
+      p2 = hand.keypoints[7];
+      fill(120, 140, 180);
+      noStroke();
+      circle(p1.x, p1.y, 10);
+      circle(p2.x, p2.y, 10);
+    }
   }
 }
